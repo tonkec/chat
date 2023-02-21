888befdab07a4346f39410723f43c97914444e53
antonijasimic <antonija1023@gmail.com>
2023-02-21 15:10:46 +0100

    add cors

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const app = express();
 const config = require("./config/app");
 const router = require("./router");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const port = config.appPort;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
