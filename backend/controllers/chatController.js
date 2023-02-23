43534c7cd724492a6fe9fe5950e6c301c90203c6
antonijasimic <antonija1023@gmail.com>
2023-02-23 17:23:54 +0100

    add create method for chat

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -4,6 +4,7 @@ const Chat = models.Chat;
 const ChatUser = models.ChatUser;
 const Message = models.Message;
 const { Op } = require("sequelize");
+const { sequelize } = require("../models");
 
 exports.index = async (req, res) => {
   const user = await User.findOne({
@@ -33,4 +34,81 @@ exports.index = async (req, res) => {
   });
 
   return res.send(user.Chats);
+};
+
+exports.create = async (req, res) => {
+  const { partnerId } = req.body;
+
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id,
+      },
+      include: [
+        {
+          model: Chat,
+          where: {
+            type: "dual",
+          },
+          include: [
+            {
+              model: ChatUser,
+              where: {
+                userId: partnerId,
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    const t = await sequelize.transaction();
+
+    if (user && user.Chats.length > 0) {
+      return res.status(403).json({
+        status: "error",
+        message: "Chat with this user already exists",
+      });
+    }
+
+    const chat = Chat.create({ type: "dual" }, { transaction: t });
+
+    await ChatUser.bulkCreate(
+      [
+        {
+          chatId: chat.id,
+          userId: req.user.id,
+        },
+        {
+          chatId: chat.id,
+          userId: partnerId,
+        },
+      ],
+      { transaction: t }
+    );
+
+    await t.commit();
+    const chats = await Chat.findOne({
+      where: {
+        id: chat.id,
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            [Op.not]: {
+              id: req.user.id,
+            },
+          },
+        },
+        {
+          model: Message,
+        },
+      ],
+    });
+    return res.send(chats);
+  } catch (e) {
+    await t.rollback();
+    return res.status(500).json({ status: "error", message: e.message });
+  }
 };
