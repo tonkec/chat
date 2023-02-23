31b97abf380443c0c490e7309edb3918828fc528
antonijasimic <antonija1023@gmail.com>
2023-02-23 16:42:58 +0100

    add references to chatUsers table

--- backend/database/migrations/20230223153022-create-chat-user.js
+++ backend/database/migrations/20230223153022-create-chat-user.js
@@ -11,17 +11,31 @@ module.exports = {
       },
       chatId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Chats",
+          key: "id",
+        },
+        onDelete: "CASCADE",
       },
       userId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
       },
     });
   },
