352fef79e0fbd6cca6b692873f34543db0a7db12
antonijasimic <antonija1023@gmail.com>
2023-02-24 11:10:28 +0100

    show last message from the first user

--- frontend/app/src/components/Friend/index.js
+++ frontend/app/src/components/Friend/index.js
@@ -1,5 +1,23 @@
-const Friend = () => {
-  return <>Friend</>;
+import { useSelector } from "react-redux";
+
+const Friend = ({ chat }) => {
+  const currentChat = useSelector((state) => state.chatReducer.currentChat);
+  const isChatOpened = () => {
+    return currentChat.id === chat.id ? "opened" : "";
+  };
+
+  const lastMessage = () => {
+    if (chat.Messages.length === 0) return "";
+
+    const message = chat.Messages[chat.Messages.length - 1];
+    return message.type === "image" ? "image" : message.message;
+  };
+  return (
+    <div className={isChatOpened()}>
+      <img width="40" height={40} src={chat.Users[0].avatar} alt="avatar" />{" "}
+      {chat.Users[0].firstName} {chat.Users[0].lastName} <p>{lastMessage()}</p>
+    </div>
+  );
 };
 
 export default Friend;
--- frontend/app/src/components/FriendList/index.js
+++ frontend/app/src/components/FriendList/index.js
@@ -1,9 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import Friend from "../Friend";
 const FriendList = () => {
+  const chats = useSelector((state) => state.chatReducer.chats);
   return (
     <div>
       <h3>Friends</h3>
       <button> Add new friend to chat with</button>
       <hr />
+
+      {chats.length > 0 ? (
+        chats.map((chat) => {
+          return <Friend chat={chat} key={chat.id} />;
+        })
+      ) : (
+        <h1>No chats</h1>
+      )}
     </div>
   );
 };
