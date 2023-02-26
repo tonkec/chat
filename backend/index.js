666305bfa9184cf9ad08d0352ec6f88bf0a0de17
antonijasimic <antonija1023@gmail.com>
2023-02-26 17:49:06 +0100

    init sockets

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,17 @@ const config = require("./config/app");
 const router = require("./router");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const http = require("http");
 const port = config.appPort;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const SocketServer = require("./socket/");
+SocketServer(server);
+
+server.listen(port, () => {
   console.log("Server listening");
 });
