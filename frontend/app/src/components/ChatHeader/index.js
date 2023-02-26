059a43e5c5d34c943d1428a7df1937b9190f4026
antonijasimic <antonija1023@gmail.com>
2023-02-26 17:10:44 +0100

    add options toggler

--- frontend/app/src/components/ChatHeader/index.js
+++ frontend/app/src/components/ChatHeader/index.js
@@ -1,5 +1,35 @@
-const ChatHeader = () => {
-  return <>ChatHeader</>;
+import { useState } from "react";
+import { userStatus } from "../../utlis/helpers";
+
+const ChatHeader = ({ chat }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  return (
+    <>
+      <div id="chatter">
+        {chat.Users.map((user) => {
+          return (
+            <div className="chatter-info">
+              <h3>
+                {user.firstName} {user.lastName}
+              </h3>
+              <div className="chatter-status">
+                <span className={`online-status ${userStatus(user)}`}></span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div onClick={() => setShowOptions(!showOptions)}>options</div>
+
+      {showOptions && (
+        <>
+          <button>Add new user</button>
+          {chat.type === "group" && <button>Leave chat</button>}
+          <button>Delete chat</button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ChatHeader;
