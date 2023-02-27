7145ccf3ad5fe170a0bf644d572b637bea95bbc6
antonijasimic <antonija1023@gmail.com>
2023-02-27 10:29:27 +0100

    add friends_online to reducer

--- frontend/app/src/store/reducers/chat.js
+++ frontend/app/src/store/reducers/chat.js
@@ -1,4 +1,10 @@
-import { FETCH_CHATS, SET_CURRENT_CHAT } from "../actions/chat";
+import {
+  FETCH_CHATS,
+  SET_CURRENT_CHAT,
+  FRIENDS_ONLINE,
+  FRIEND_OFFLINE,
+  FRIEND_ONLINE,
+} from "../actions/chat";
 const initilState = {
   chats: [],
   currentChat: {},
@@ -17,6 +23,27 @@ const chatReducer = (state = initilState, action) => {
       return {
         ...state,
         currentChat: payload,
+      };
+    case FRIENDS_ONLINE:
+      const chatCopy = state.chats.map((chat) => {
+        return {
+          ...chat,
+          Users: chat.Users.map((user) => {
+            if (payload.includes(user.id)) {
+              return {
+                ...user,
+                status: "online",
+              };
+            }
+
+            return user;
+          }),
+        };
+      });
+
+      return {
+        ...state,
+        chats: chatCopy,
       };
     default: {
       return state;
