8bb399c1db39bfb2a0aa8a4629308e9b826c21d7
antonijasimic <antonija1023@gmail.com>
2023-02-28 07:33:43 +0100

    fix typo

--- backend/router/chat.js
+++ backend/router/chat.js
@@ -6,11 +6,14 @@ const {
   deleteChat,
   imageUpload,
 } = require("../controllers/chatController");
+const { validate } = require("../validators");
 const { auth } = require("../middleware/auth");
-const chatFile = require("../middleware/fileUpload");
+const { chatFile } = require("../middleware/fileUpload");
+
 router.get("/", [auth], index);
 router.get("/messages", [auth], messages);
 router.post("/create", [auth], create);
 router.post("/upload-image", [auth, chatFile], imageUpload);
 router.delete("/:id", [auth], deleteChat);
+
 module.exports = router;
