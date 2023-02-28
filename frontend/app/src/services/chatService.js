97f2c088220221aab9af06f0a0164e4cd21869cb
antonijasimic <antonija1023@gmail.com>
2023-02-28 07:41:44 +0100

    add new service for image upload on FE

--- frontend/app/src/services/chatService.js
+++ frontend/app/src/services/chatService.js
@@ -8,6 +8,16 @@ const ChatService = {
         throw e;
       });
   },
+  uploadImage: (data) => {
+    const headers = {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    };
+    return API.post("/chats/upload-image", data, headers)
+      .then((res) => res.data.url)
+      .catch((e) => {
+        throw e;
+      });
+  },
 };
 
 export default ChatService;
