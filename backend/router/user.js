8ed83bb85b7b4545425c050aae5f5e168c24c27b
antonijasimic <antonija1023@gmail.com>
2023-03-02 07:28:01 +0100

    add new route for searching users

--- backend/router/user.js
+++ backend/router/user.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { update } = require("../controllers/usersController");
+const { update, search } = require("../controllers/usersController");
 const { auth } = require("../middleware/auth");
 const { userFile } = require("../middleware/fileUpload");
 const { validate } = require("../validators/index");
 const { rules: updateRules } = require("../validators/user/update");
 router.post("/update", [auth, userFile, updateRules(), validate], update);
+router.get("/search-users", auth, search);
 module.exports = router;
