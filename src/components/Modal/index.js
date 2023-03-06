33f4b802e06e5295389bd9058b2379e66d6c00fe
antonijasimic <antonija1023@gmail.com>
2023-03-06 09:17:21 +0100

    remove unused vars

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,16 +1,17 @@
-import React from "react";
-import "./Modal.scss";
+import React from 'react';
+import './Modal.scss';
 
 const Modal = (props) => {
   const findByKey = (name) =>
     props.children.map((child) => {
       if (child.key === name) return child;
+      return null;
     });
 
   const closeModal = (e) => {
     e.stopPropagation();
 
-    if (e.target.classList.contains("modal-close")) {
+    if (e.target.classList.contains('modal-close')) {
       return props.click();
     }
   };
@@ -19,15 +20,15 @@ const Modal = (props) => {
     <div className="modal-mask modal-close" onClick={closeModal}>
       <div className="modal-wrapper">
         <div className="modal-container">
-          <div className="modal-header">{findByKey("header")}</div>
+          <div className="modal-header">{findByKey('header')}</div>
 
-          <div className="modal-body">{findByKey("body")}</div>
+          <div className="modal-body">{findByKey('body')}</div>
 
           <div className="modal-footer">
             <button className="modal-close" onClick={closeModal}>
               CLOSE
             </button>
-            {findByKey("footer")}
+            {findByKey('footer')}
           </div>
         </div>
       </div>
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,20 +1,14 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchChats } from "../../store/actions/chat";
-import Messenger from "../Messenger";
-import FriendList from "../FriendList";
-import Navbar from "../Navbar";
-import "./Chat.scss";
-import useSocket from "../../hooks/socketConnect";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Messenger from '../Messenger';
+import FriendList from '../FriendList';
+import Navbar from '../Navbar';
+import './Chat.scss';
+import useSocket from '../../hooks/socketConnect';
 const Chat = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.authReducer.user);
   useSocket(dispatch, user);
-  // useEffect(() => {
-  //   dispatch(fetchChats())
-  //     .then((res) => res)
-  //     .catch((e) => console.log(e));
-  // }, [dispatch]);
 
   return (
     <div id="chat-container">
