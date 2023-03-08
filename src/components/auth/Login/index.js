039f8767a0b8e7148d1e644f93df6fc39af52eb8
antonijasimic <antonija1023@gmail.com>
2023-03-08 20:10:19 +0100

    style login page

---
+++ src/components/auth/Login/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { login } from '../../../store/actions/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import AuthLayout from '../../Layout/AuthLayout';
+import './Login.scss';
+
+const Login = () => {
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isVerified = useSelector((state) => {
+    return state.authReducer.isVerified;
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ email, password }, navigate));
+  };
+
+  const message = isVerified !== false ? '' : 'Email not verified';
+
+  return (
+    <AuthLayout>
+      <form onSubmit={onSubmit} className="form-auth">
+        <h3 className="form-heading">Ulogiraj se!</h3>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+          type="password"
+          placeholder="Lozinka"
+        />
+        <button>Login</button>
+      </form>
+      <div className="links-auth">
+        {message && <p>{message}</p>}
+        <Link to="/register">Registriraj se</Link> {'  '}
+        <Link to="/forgot-password">Zaboravljena lozinka?</Link>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
