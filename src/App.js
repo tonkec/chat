16d1c40ba8d8a3bdf579878e293c80b5b1f1985d
antonijasimic <antonija1023@gmail.com>
2023-03-08 09:38:33 +0100

    move routes to its own component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import './App.scss';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Chat from './components/chat/Chat';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from './components/NotFound';
-import ProtectedRoute from './router/ProtectedRoute';
-import ForgotPassword from './components/auth/ForgotPassword';
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<ProtectedRoute />}>
-          <Route exact path="/" element={<Chat />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-}
+import RoutesComponent from './router';
+const App = () => <RoutesComponent />;
 
 export default App;
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Login from './../components/auth/Login';
+import Register from './../components/auth/Register';
+import Chat from './../components/chat/Chat';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './../components/NotFound';
+import ProtectedRoute from './ProtectedRoute';
+import ForgotPassword from './../components/auth/ForgotPassword';
+const RoutesComponent = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<ProtectedRoute />}>
+        <Route exact path="/" element={<Chat />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
+
+export default RoutesComponent;
