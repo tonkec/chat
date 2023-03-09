5c0bc44c57299c863ec64c3667b948f771896f35
antonijasimic <antonija1023@gmail.com>
2023-03-09 08:04:39 +0100

    add index to the end of the file path

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Messenger from '../Messenger';
 import FriendList from '../FriendList';
 import './Chat.scss';
 import useSocket from '../../hooks/socketConnect';
--- src/pages/MessagesPage/index.js
+++ src/pages/MessagesPage/index.js
@@ -1,4 +1,4 @@
-import Chat from './../../components/Chat';
+import Chat from './../../components/Chat/index';
 import Main from './../../components/Layout/Main';
 const MessagesPage = () => <Main component={Chat} />;
 
