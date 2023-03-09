8d59c4243b2bf5740f1e291496e7db16e0f83f4d
antonijasimic <antonija1023@gmail.com>
2023-03-09 08:10:17 +0100

    readd chat components

---
+++ src/components/Chat/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import FriendList from '../FriendList';
+import './Chat.scss';
+import useSocket from '../../hooks/socketConnect';
+const Chat = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.authReducer.user);
+  useSocket(dispatch, user);
+
+  return <FriendList />;
+};
+
+export default Chat;
