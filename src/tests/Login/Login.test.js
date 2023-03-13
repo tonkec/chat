290250ef115e4e3d1eb203d276165a8ead6e1a12
antonijasimic <antonija1023@gmail.com>
2023-03-13 10:12:57 +0100

    remove myCustomRender helper

--- src/tests/Login/Login.test.js
+++ src/tests/Login/Login.test.js
@@ -1,9 +1,16 @@
 import { screen } from '@testing-library/react';
-import App from '../../App';
-import { myCustomRender } from './helper';
 import userEvent from '@testing-library/user-event';
 import { fireEvent } from '@testing-library/react';
 import { EMAIL_NOT_VERIFIED } from '../../components/auth/constants';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import FlashMessage from '../../components/FlashMessage';
+import FlashMessageProvider from '../../context/FlashMessage/flashMessageProvider';
+import appStore from '../../store/index';
+import Login from '../../components/auth/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from '../../router/ProtectedRoute';
+import HomePage from '../../pages/HomePage';
 const verifiedUser = {
   email: 'antonija1023@gmail.com',
   password: 'glitch',
@@ -19,18 +26,34 @@ const wrongCredentialsUser = {
   password: 'secret',
 };
 
+const App = () => (
+  <Provider store={appStore}>
+    <FlashMessageProvider>
+      <FlashMessage />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<ProtectedRoute />}>
+            <Route exact path="/" element={<HomePage />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </FlashMessageProvider>
+  </Provider>
+);
+
 test('renders the login page', async () => {
-  myCustomRender(<App />);
+  render(<App />);
   expect(screen.getByText('Ulogiraj se!')).toBeTruthy();
 });
 
 test('should not show the email not verified message on mount', async () => {
-  myCustomRender(<App />);
+  render(<App />);
   expect(screen.queryByText('Email not verified')).toBeFalsy();
 });
 
 test('show email not verified message', async () => {
-  myCustomRender(<App />);
+  render(<App />);
   const inputEmail = screen.getByPlaceholderText('Email');
   fireEvent.change(inputEmail, { target: { value: notVerifiedUser.email } });
 
@@ -43,7 +66,7 @@ test('show email not verified message', async () => {
 });
 
 test('should not log in with the wrong credentials', async () => {
-  myCustomRender(<App />);
+  render(<App />);
 
   const inputEmail = screen.getByPlaceholderText('Email');
   fireEvent.change(inputEmail, {
@@ -59,7 +82,7 @@ test('should not log in with the wrong credentials', async () => {
 });
 
 test('not showing of email not verified message and should log in with the correct credentials', async () => {
-  myCustomRender(<App />);
+  render(<App />);
   const inputEmail = screen.getByPlaceholderText('Email');
   fireEvent.change(inputEmail, { target: { value: verifiedUser.email } });
 
