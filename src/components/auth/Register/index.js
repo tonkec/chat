c0819139f10ee1965c6d77235ae56b0efacc5265
antonijasimic <antonija1023@gmail.com>
2023-03-27 11:39:56 +0200

    disable button while error is present

--- src/components/auth/Register/index.js
+++ src/components/auth/Register/index.js
@@ -1,101 +1,114 @@
-import React, { useContext, useState } from "react";
-import { register } from "../../../store/actions/auth";
-import { useDispatch } from "react-redux";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import { register } from '../../../store/actions/auth';
+import { useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
 
-import AuthLayout from "../../Layout/AuthLayout";
-import "./../Auth.scss";
-import isEmailValid from "../validators/emailValidator";
-import isPasswordValid from "../validators/passwordValidator";
+import AuthLayout from '../../Layout/AuthLayout';
+import './../Auth.scss';
+import isEmailValid from '../validators/emailValidator';
+import isPasswordValid from '../validators/passwordValidator';
 import {
   EMAIL_INVALID,
   NAME_EMPTY,
   LAST_NAME_EMPTY,
   PASSWORD_MIN_CHARACTERS,
-} from "../constants/login";
+} from '../constants/login';
 
-import nameValidator from "../validators/nameValidator";
-import optionValidator from "../validators/optionValidator";
-import FlashMessageContext from "../../../context/FlashMessage/flashMessageContext";
+import nameValidator from '../validators/nameValidator';
+import optionValidator from '../validators/optionValidator';
+import FlashMessageContext from '../../../context/FlashMessage/flashMessageContext';
 const Register = () => {
   let navigate = useNavigate();
   const dispatch = useDispatch();
   const flashMessageContext = useContext(FlashMessageContext);
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [gender, setGender] = useState("female");
-  const [error, setError] = useState("");
-  const [isDisabled, setDisabled] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [gender, setGender] = useState('female');
+  const [error, setError] = useState('');
+  const [isDisabled, setDisabled] = useState(true);
 
   const handleValidInput = (action, value) => {
     switch (action) {
-      case "email": {
+      case 'email': {
         setEmail(value);
         break;
       }
-      case "password": {
+      case 'password': {
         setPassword(value);
         break;
       }
-      case "firstName": {
+      case 'firstName': {
         setFirstName(value);
         break;
       }
-      case "lastName": {
+      case 'lastName': {
         setLastName(value);
         break;
       }
 
-      case "gender": {
+      case 'gender': {
         setGender(value);
         break;
       }
       default: {
-        console.log("Invalid value for validation type");
+        console.log('Invalid value for validation type');
       }
     }
     flashMessageContext.close();
     setError(null);
-    setDisabled(false);
+
+    if (
+      email !== '' &&
+      firstName !== '' &&
+      lastName !== '' &&
+      password !== ''
+    ) {
+      setDisabled(false);
+    }
   };
+
   const handleInvalidInput = (error) => {
     flashMessageContext.error(error);
     setError(error);
     setDisabled(true);
   };
+
   const onNameChange = (e) => {
     const value = e.target.value;
     const validName = nameValidator(value);
     if (validName) {
-      handleValidInput("firstName", value);
+      handleValidInput('firstName', value);
       return;
     }
     handleInvalidInput(NAME_EMPTY);
   };
+
   const onLastNameChange = (e) => {
     const value = e.target.value;
     const validLastName = nameValidator(value);
     if (validLastName) {
-      handleValidInput("lastName", value);
+      handleValidInput('lastName', value);
       return;
     }
     handleInvalidInput(LAST_NAME_EMPTY);
   };
+
   const onGenderSelect = (e) => {
     const value = e.target.value;
     const gender = optionValidator(value);
     if (gender) {
-      handleValidInput("gender", value);
+      handleValidInput('gender', value);
     }
   };
+
   const onEmailChange = (e) => {
     const value = e.target.value;
     const validEmail = isEmailValid(value);
     if (validEmail) {
-      handleValidInput("email", value);
+      handleValidInput('email', value);
       return;
     }
     handleInvalidInput(EMAIL_INVALID);
@@ -105,7 +118,7 @@ const Register = () => {
     const value = e.target.value;
     const validPassword = isPasswordValid(value);
     if (validPassword) {
-      handleValidInput("password", value);
+      handleValidInput('password', value);
       return;
     }
     handleInvalidInput(PASSWORD_MIN_CHARACTERS);
@@ -113,9 +126,13 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    dispatch(
-      register({ email, password, firstName, lastName, gender }, navigate)
-    );
+    const errorIsEmpty = error === '' && !isDisabled;
+    if (errorIsEmpty) {
+      dispatch(
+        register({ email, password, firstName, lastName, gender }, navigate)
+      );
+      return;
+    }
   };
   return (
     <AuthLayout>
