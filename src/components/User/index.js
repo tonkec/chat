96fe3f2b645a5fa3c2bc8b7938931c13f92e6978
antonijasimic <antonija1023@gmail.com>
2023-04-03 18:09:34 +0200

    show different data on user profile page

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -14,9 +14,29 @@ export const User = () => {
   }, [dispatch, userId]);
   return (
     user && (
-      <div className="user">
-        <img src={user.avatar} alt="user avatar" />
-        <h1>{user.firstName}</h1>
+      <div className="user-wrapper">
+        <div className="user">
+          <div className="user-name">
+            <img src={user.avatar} alt="user avatar" />
+            <div>
+              <h4>{user.firstName}</h4>
+              <p>
+                {user.location}, {user.age}
+              </p>
+            </div>
+          </div>
+
+          <div className="user-identity">
+            <div>
+              <span>{user.sexuality}</span>, <span>{user.gender}</span>
+            </div>
+          </div>
+          <p className="user-bio">
+            <b>Bio:</b> <br /> {user.bio}
+          </p>
+        </div>
+
+        <div>Photo album</div>
       </div>
     )
   );
