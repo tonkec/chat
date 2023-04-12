fa9c96779f025209e8c97b3a89cf6b57c7d1f516
antonijasimic <antonija1023@gmail.com>
2023-04-12 08:59:14 +0200

    remove unused var

--- src/tests/pages/UserPage/UserPage.test.js
+++ src/tests/pages/UserPage/UserPage.test.js
@@ -1,4 +1,4 @@
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import FlashMessage from '../../../components/FlashMessage';
 import FlashMessageProvider from '../../../context/FlashMessage/flashMessageProvider';
