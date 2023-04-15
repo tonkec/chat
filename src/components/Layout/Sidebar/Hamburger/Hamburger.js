6bf6de3c4386bec000b91f56a9a7ec6d7bae443c
evakalo <99054186+evakalo@users.noreply.github.com>
2023-04-15 19:56:02 +0200

    added a new feature - hamburger navbar
    or screens smaller than 992px, hamburger navbar will apear and it will change the style of sidebar

---
+++ src/components/Layout/Sidebar/Hamburger/Hamburger.js
@@ -0,0 +1,12 @@
+import "./Hamburger.css";
+export default function Hamburger() {
+  return (
+    <>
+      <div className="hamburger active close-burger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </>
+  );
+}
--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,11 +1,26 @@
-import Links from './Links';
-import Logo from './Logo';
-import './Sidebar.scss';
+import Links from "./Links";
+import Logo from "./Logo";
+import "./Sidebar.scss";
+import Hamburger from "./Hamburger/Hamburger";
+import { useState } from "react";
 const Sidebar = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const [viewBurger, setViewBurger] = useState(true);
+  function showLinks(e) {
+    setOpenSidebar(!openSidebar);
+    setViewBurger(!viewBurger);
+  }
+
   return (
     <aside className="sidebar">
       <Logo />
-      <Links />
+      <div onClick={showLinks}>
+        <Hamburger />
+      </div>
+
+      <div className={`nav-links  ${openSidebar && "active"}`}>
+        <Links />
+      </div>
     </aside>
   );
 };
